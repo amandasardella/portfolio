d282e2080893a708b9c9c5e69767de26311c2de5
Amanda Sardella <amandansardella@gmail.com>
2025-07-01 23:06:50 -0400

    added tailwind theme variables, animations, and utility classes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+    return (
+        <div>
+            {/* theme toggle */}
+
+            {/* background effects */}
+
+            {/* navbar */}
+
+            {/* main content */}
+
+
+            {/* footer */}
+        </div>
+    );
+};
